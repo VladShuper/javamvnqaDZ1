a98c74bc8a0279c2a9e19c0f1be8ac7727400c85
Vlad Shuper <cristianbox777@gmail.com>
2024-06-26 12:41:40 +0300

    git.mvnDZ1

--- src/main/java/ru/netology/javaqa/Main.java
+++ src/main/java/ru/netology/javaqa/Main.java
@@ -1,7 +1,13 @@
-package ru.netology.javaqa;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BonusService service = new BonusService();
+
+        int expected = 150;
+        long actual = service.calculate(5_000, true);
+        System.out.println("1." + expected + "==?==" + actual);
+
+        expected = 50;
+        actual = service.calculate(5_000, false);
+        System.out.println("2." + expected + "==?==" + actual);
     }
-}
+}n
